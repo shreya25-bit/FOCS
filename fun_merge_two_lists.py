56d267f369e32da728948e683c8d8d01cf9ab875
shreya25-bit <shreya3106.beai25@chitkara.edu.in>
2025-12-17 15:40:04 +0530

    Program5

---
+++ fun_merge_two_lists.py
@@ -0,0 +1,8 @@
+def merge_two_lists(list1, list2):
+    """Merge two lists into one."""
+    return list1 + list2       
+# Example usage:
+list1 = [1, 2, 3]
+list2 = [4, 5, 6]   
+merged_list = merge_two_lists(list1, list2)
+print("Merged list:", merged_list)  
